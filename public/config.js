807a94de40055d93b66c90a957dd694780b75be1
wurh <wurh@users.noreply.github.com>
2015-05-19 14:40:49 +0800

    [UPD]增加配置项

---
+++ public/config.js
@@ -0,0 +1,8 @@
+var config ={
+    page2:'http://www.baidu.com',
+    page3:'http://www.sina.com',
+    page4:'http://www.51job.com',
+    page5:'http://www.youku.com',
+    page6:'http://www.tudou.com',
+    page7:'http://www.taobao.com'
+}
--- public/huodong1.js
+++ public/huodong1.js
@@ -1,1 +1,24 @@
-(function(){var swiper=new Swiper(".swiper-container",{pagination:".swiper-pagination",paginationClickable:true,loop:true,direction:"vertical",onSlideChangeEnd:onSlider})})();function onSlider(swiper){var dataP=parseInt($(".swiper-slide-active").attr("dataP"));if(dataP>1){var prePage=dataP-1;$(".page"+prePage+"wrap").hide()}else{var prePage=dataP+1;$(".page"+prePage+"wrap").hide()}$(".page"+dataP+"wrap").show()};
+(function () {
+    init();
+    var swiper = new Swiper(".swiper-container", {pagination: ".swiper-pagination", paginationClickable: true, loop: true, direction: "vertical", onSlideChangeEnd: onSlider})
+})();
+function onSlider(swiper) {
+    var dataP = parseInt($(".swiper-slide-active").attr("dataP"));
+    if (dataP > 1) {
+        var prePage = dataP - 1;
+        $(".page" + prePage + "wrap").hide()
+    } else {
+        var prePage = dataP + 1;
+        $(".page" + prePage + "wrap").hide()
+    }
+    $(".page" + dataP + "wrap").show()
+};
+
+function init(){
+    $('#page2btn').attr('href',config.page2);
+    $('#page3btn').attr('href',config.page3);
+    $('#page4btn').attr('href',config.page4);
+    $('#page5btn').attr('href',config.page5);
+    $('#page6btn').attr('href',config.page6);
+    $('#page7btn').attr('href',config.page7);
+}
