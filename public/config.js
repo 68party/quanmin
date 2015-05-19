6afd336f825a2a2e214ba3b36c90f205926e1abf
wurh <wurh@users.noreply.github.com>
2015-05-19 15:25:41 +0800

    [UPD]更新配置地址

--- public/config.js
+++ public/config.js
@@ -1,8 +1,8 @@
 var config ={
-    page2:'http://www.baidu.com',
-    page3:'http://www.sina.com',
-    page4:'http://www.51job.com',
-    page5:'http://www.youku.com',
-    page6:'http://www.tudou.com',
-    page7:'http://www.taobao.com'
+    page2:'http://www.68party.com/',
+    page3:'http://www.68party.com/',
+    page4:'http://www.68party.com/',
+    page5:'http://www.68party.com/',
+    page6:'http://www.68party.com/',
+    page7:'http://www.68party.com/'
 }
